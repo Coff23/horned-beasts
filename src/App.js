726069b45cf4983acbabda2a87c84e13a7a9008c
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-04-10 15:59:27 -0500

    lab 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Header creator={"Joshua Coffey"} />
+      <Header />
       <Main />
       <Footer author={"Joshua Coffey"}/>
     </div>
