638b01507444a1f2266ffc0454324038fbc0b590
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-04-11 22:34:33 -0500

    stretch goals

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,25 @@
 import { Component } from "react";
 
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "footer": "Joshua Coffey"
+        }
+    }
+
+    handleClick = () => {
+        const newFooter = this.state.footer === "Coffey, Joshua" ? "Joshua Coffey" : "Coffey, Joshua";
+        this.setState({
+            footer: newFooter
+        });
+    }
     render() {
-        return <footer>
-            <p> {this.props.author} </p>
+        return <footer onClick={this.handleClick}>
+            <p> {this.state.footer} </p>
         </footer>
     }
+
 }
 
 export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import { Component } from "react";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "header": "Beast Judge"
+        }
+    }
+
+    handleClick = () => {
+        const newHeader = this.state.header === "Vote!" ? "Beast Judge" : "Vote!";
+        this.setState({
+            header: newHeader
+        });
+    }
+
     render() {
-        return <h1> {this.props.title} </h1>
+        return <h1 onClick={this.handleClick}> {this.state.header} </h1>
     }
 }
 
