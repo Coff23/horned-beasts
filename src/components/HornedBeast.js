2a07fe082a184e9e68777bb2d7223108a4996ff2
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-04-12 18:34:25 -0500

    modal

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,18 +1,52 @@
 import { Component } from "react";
-import Image from 'react-bootstrap/Image';
+import Image from "react-bootstrap/Image";
 import FavoritedImage from "./FavortitedImage";
 import DislikeImage from "./DislikeImage";
+import BeastModal from "./modal";
 
 class HornedBeast extends Component {
+  state = {
+    showModal: false,
+  };
+
+  handleShow = () => {
+    this.setState({
+      showModal: true,
+    });
+  };
+
+  handleClose = () => {
+    console.log("close");
+    this.setState({
+      showModal: false,
+    });
+  };
+
   render() {
     return (
       <>
-        <h2>{this.props.title}</h2>
-        <Image src={this.props.imageUrl} alt={this.props.description} title={this.props.title} key={this.props.keyword} fluid rounded></Image>
+        <div onClick={this.handleShow}>
+          <h2>{this.props.title}</h2>
+          <Image
+            src={this.props.imageUrl}
+            alt={this.props.description}
+            title={this.props.title}
+            key={this.props.keyword}
+            fluid
+            rounded
+          ></Image>
+        </div>
         <FavoritedImage />
         <DislikeImage />
+        <BeastModal
+          title={this.props.title}
+          imageUrl={this.props.imageUrl}
+          description={this.props.description}
+          show={this.state.showModal}
+          onClose={this.handleClose}
+        />
       </>
-    )
+    );
   }
 }
 
